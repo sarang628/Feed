f2c76caa60a10ed47dce9f42ecc72a8d200e12d9
SRYANG <sarang628@naver.com>
2023-07-07 18:23:13 +0800

    update

---
+++ library/src/main/java/com/example/screen_feed/test/PreviewFeedScreen.kt
@@ -0,0 +1,44 @@
+package com.example.screen_feed.test
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.screen_feed.FeedsScreen
+import com.example.screen_feed.data.Feed
+import com.sarang.base_feed.uistate.FeedsScreenUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@Preview
+@Composable
+fun PreviewFeedScreen() {
+    val data = MutableStateFlow(FeedsScreenUiState(
+        feeds = ArrayList<Feed>().apply {
+            add(
+                Feed(
+                    name = "monkey",
+                    restaurantName = "Mcdonalds",
+                    isFavorite = true,
+                    isLike = true,
+                    rating = 3.5f,
+                    likeAmount = 10,
+                    commentAmount = 10,
+                    author = "bb",
+                    author1 = "cc",
+                    author2 = "dd",
+                    comment = "aaaa",
+                    comment1 = "bbbb",
+                    comment2 = "cccc",
+                    reviewImages = ArrayList<String>().apply {
+                        add("")
+                        add("")
+                        add("")
+                    },
+                    contents = "abcd"
+                )
+            )
+        }
+    ))
+
+
+
+    FeedsScreen(uiStateFlow = data, inputEvents = null)
+}
