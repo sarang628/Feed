19144594c81f90413c5a137d264ab11b8448d940
SRYANG <sarang628@naver.com>
2023-11-02 12:54:40 +0800

    update

--- library/src/main/java/com/sryang/torang/compose/FeedsScreen.kt
+++ library/src/main/java/com/sryang/torang/compose/FeedsScreen.kt
@@ -1,12 +1,10 @@
 package com.sryang.torang.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -14,11 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.sryang.torang.R
 import com.sryang.torang.uistate.FeedUiState
 import com.sryang.torang.viewmodels.FeedsViewModel
 
