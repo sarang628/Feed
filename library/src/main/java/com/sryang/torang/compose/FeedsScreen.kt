6081d2963bb4ca1abbb0f694e72c6731550c0b86
SRYANG <sarang628@naver.com>
2023-11-02 13:15:54 +0800

    update

--- library/src/main/java/com/sryang/torang/compose/FeedsScreen.kt
+++ library/src/main/java/com/sryang/torang/compose/FeedsScreen.kt
@@ -2,7 +2,7 @@ package com.sryang.torang.compose
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
