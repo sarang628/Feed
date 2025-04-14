7b0bfef24e17f2ba14ff46d5a9140f3bf325266f
sryang <sarang628@gmail.com>
2025-04-14 22:36:33 +0900

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.sarang.torang
 
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,11 +21,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.samples.apps.sunflower.ui.TorangTheme
 import com.sarang.torang.repository.FeedRepository
 import com.sarang.torang.repository.LoginRepository
 import com.sarang.torang.repository.ProfileRepository
-import com.sryang.library.pullrefresh.rememberPullToRefreshState
+import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -43,8 +42,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.requestFeature(Window.FEATURE_ACTION_BAR);
-        getActionBar()?.hide();
+
+        enableEdgeToEdge()
 
         setContent {
             var reviewId by remember { mutableStateOf("245") }
---
+++ app/src/main/java/com/sarang/torang/RootNavController.kt
@@ -0,0 +1,67 @@
+package com.sarang.torang
+
+import androidx.navigation.NavHostController
+
+class RootNavController(val navController: NavHostController) {
+    fun modReview(): (Int) -> Unit = { }
+
+    fun imagePager(reviewId: Int, position: Int) {
+
+    }
+
+    fun emailLogin() {
+
+    }
+
+    fun restaurant(it: Int) {
+
+    }
+
+    fun settings() {
+
+    }
+
+    fun popBackStack() {
+
+    }
+
+    fun like(id: Int) {
+
+    }
+
+    fun main() {
+
+    }
+
+    fun restaurantImagePager(it: Int) {
+
+    }
+
+    fun addReview() {
+
+    }
+
+    fun splash() {
+
+    }
+
+    fun editProfileImage() {
+
+    }
+
+    fun singleTopLogin() {
+
+    }
+
+    fun singleTopMain() {
+
+    }
+
+    fun review(it: Int) {
+
+    }
+
+    fun profile(it: Int) {
+
+    }
+}
