5c73c3de1ca39bcb71b554c170ea665b46abcd8b
sryang <sarang628@gmail.com>
2025-04-16 10:12:49 +0900

    remove log

--- library/src/main/java/com/sarang/torang/data/feed/FeedImage.kt
+++ library/src/main/java/com/sarang/torang/data/feed/FeedImage.kt
@@ -1,11 +1,6 @@
 package com.sarang.torang.data.feed
 
-import android.util.Log
 import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.dp
-
-
-private val tag: String = "__FeedImage"
 
 data class FeedImage(
     val url: String,
@@ -22,26 +17,14 @@ fun FeedImage.adjustHeight(density: Density, screenWidthDp: Int, screenHeightDp:
         val imageAspectRatio = widthDp.toFloat() / heightDp.toFloat()
         val screenAspectRatio = screenWidthDp.toFloat() / screenHeightDp.toFloat()
 
-        Log.d(tag, "screen resolution : $screenWidthDp * $screenHeightDp")
-        Log.d(tag, "picture resolution : $widthDp * $heightDp")
-
-
-
-
         if (width > 0 && height > 0) {
             if (imageAspectRatio > screenAspectRatio) {
-                // 이미지가 화면보다 더 넓은 경우: 가로를 기준으로 크기를 맞춤
-                //newWidth = screenWidthDp
-                newHeight = (screenWidthDp / imageAspectRatio).toInt()
+                newHeight =
+                    (screenWidthDp / imageAspectRatio).toInt() // 이미지가 화면보다 더 넓은 경우: 가로를 기준으로 크기를 맞춤
             } else {
-                // 이미지가 화면보다 더 높은 경우: 세로를 기준으로 크기를 맞춤
-                newHeight = screenHeightDp
-                //newWidth = (screenHeightDp * imageAspectRatio).toInt()
+                newHeight = screenHeightDp // 이미지가 화면보다 더 높은 경우: 세로를 기준으로 크기를 맞춤
             }
-
         }
-
-        Log.d(tag, "new height : $newHeight")
     }
 
     return newHeight
