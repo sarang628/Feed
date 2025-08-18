c4cf8e15e792b6b7919ca676853a104d27f642ac
sryang <sarang628@gmail.com>
2025-08-19 07:18:09 +0800

    remove todo

--- library/src/main/java/com/sarang/torang/compose/feed/FeedScreenInMain.kt
+++ library/src/main/java/com/sarang/torang/compose/feed/FeedScreenInMain.kt
@@ -146,7 +146,7 @@ fun PreviewMainFeedScreen() {
                 )
             ),
         ),
-        onAddReview = { /*TODO*/ },
+        onAddReview = {},
         consumeErrorMessage = {},
         onRefresh = {},
         onBottom = {},
