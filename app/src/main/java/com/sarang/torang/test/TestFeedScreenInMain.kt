c7fa2acef88e24bc43dfa5a77d53c98f5d7a4402
sryang <sarang628@gmail.com>
2025-08-19 10:48:32 +0800

    transfer LocalPullToRefreshLayoutType

--- app/src/main/java/com/sarang/torang/test/TestFeedScreenInMain.kt
+++ app/src/main/java/com/sarang/torang/test/TestFeedScreenInMain.kt
@@ -8,10 +8,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.sarang.torang.compose.feed.FeedScreenInMain
+import com.sarang.torang.compose.feed.LocalPullToRefreshLayoutType
 import com.sarang.torang.compose.feed.component.LocalBottomDetectingLazyColumnType
 import com.sarang.torang.compose.feed.component.LocalFeedCompose
 import com.sarang.torang.di.feed_di.CustomBottomDetectingLazyColumnType
 import com.sarang.torang.di.feed_di.CustomFeedCompose
+import com.sarang.torang.di.feed_di.CustomLocalPullToRefreshType
 import com.sryang.library.pullrefresh.PullToRefreshLayoutState
 import com.sryang.library.pullrefresh.rememberPullToRefreshState
 
@@ -24,7 +26,8 @@ fun TestFeedScreenForMain(
 ) {
     var onTop by remember { mutableStateOf(false) }
     CompositionLocalProvider(LocalFeedCompose provides CustomFeedCompose,
-        LocalBottomDetectingLazyColumnType provides CustomBottomDetectingLazyColumnType
+        LocalBottomDetectingLazyColumnType provides CustomBottomDetectingLazyColumnType,
+        LocalPullToRefreshLayoutType provides CustomLocalPullToRefreshType
     ) {
         FeedScreenInMain(
             scrollToTop = onTop,
