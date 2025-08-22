27d065c8aa72bec4aa74624f90033b1d89a12779
sryang <sarang628@gmail.com>
2025-08-22 21:22:56 +0800

    add Local feed ui

--- library/src/main/java/com/sarang/torang/compose/feed/component/LocalFeed.kt
+++ library/src/main/java/com/sarang/torang/compose/feed/component/LocalFeed.kt
@@ -1,18 +1,42 @@
 package com.sarang.torang.compose.feed.component
 
 import android.util.Log
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sarang.torang.R
 import com.sarang.torang.compose.feed.feedType
 
 val LocalFeedCompose = compositionLocalOf<feedType> {
     @Composable { feed, _, _, _, _, _, _ ->
         Log.w("__LocalFeedCompose", "feed compose isn't set")
-        Row {
-            Text(feed.name)
-            Text(feed.restaurantName)
+        FeedSample()
+    }
+}
+
+@Composable
+@Preview
+fun FeedSample() {
+    Box {
+        Column(modifier = Modifier.height(300.dp)) {
+            OutlinedCard(modifier = Modifier.fillMaxWidth().height(200.dp)) {
+                Column {
+                    Text("name")
+                    Text("restaurants")
+                    Image(modifier = Modifier.fillMaxWidth().height(200.dp) ,painter = painterResource(R.drawable.ic_add), contentDescription = "")
+                }
+            }
+            Text("contents")
         }
     }
 }
