6b482a49a6dc2acc1aaf5f7ef834cfeec1840c8d
sryang <sarang628@gmail.com>
2025-09-05 16:02:29 +0800

    refactor : add LocalFeed design

--- library/src/main/java/com/sarang/torang/compose/feed/component/LocalFeed.kt
+++ library/src/main/java/com/sarang/torang/compose/feed/component/LocalFeed.kt
@@ -1,16 +1,23 @@
 package com.sarang.torang.compose.feed.component
 
 import android.util.Log
+import android.widget.RatingBar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,16 +34,20 @@ val LocalFeedCompose = compositionLocalOf<feedType> {
 @Composable
 @Preview
 fun FeedSample() {
-    Box {
-        Column(modifier = Modifier.height(300.dp)) {
-            OutlinedCard(modifier = Modifier.fillMaxWidth().height(200.dp)) {
-                Column {
-                    Text("name")
-                    Text("restaurants")
-                    Image(modifier = Modifier.fillMaxWidth().height(200.dp) ,painter = painterResource(R.drawable.ic_add), contentDescription = "")
+    Column {
+        OutlinedCard(modifier = Modifier.fillMaxWidth()) {
+            Column {
+                Row(Modifier.padding(8.dp)) {
+                    Image(modifier = Modifier.size(40.dp).clip(CircleShape) ,painter = painterResource(R.drawable.sample), contentDescription = "", contentScale = ContentScale.Crop)
+                    Column {
+                        Text("name")
+                        Text("restaurants")
+                    }
                 }
+                Image(modifier = Modifier.fillMaxWidth().height(300.dp) ,painter = painterResource(R.drawable.sample), contentDescription = "", contentScale = ContentScale.Crop)
+                Text(modifier = Modifier.padding(8.dp), text = "contents")
             }
-            Text("contents")
         }
+
     }
 }
