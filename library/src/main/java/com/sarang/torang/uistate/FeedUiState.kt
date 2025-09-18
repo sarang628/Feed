b6b0a437b2138ee5cb09e17fe987cb528530434c
sryang <sarang628@gmail.com>
2025-09-18 08:57:17 +0800

    arrange code

--- library/src/main/java/com/sarang/torang/uistate/FeedUiState.kt
+++ library/src/main/java/com/sarang/torang/uistate/FeedUiState.kt
@@ -8,11 +8,11 @@ import java.util.Objects
 
 // @formatter:off
 sealed interface FeedUiState {
-    data class  Error(val msg: String?)       : FeedUiState
-    data class  Success(val list: List<Feed>) : FeedUiState
-    object      Loading                       : FeedUiState
-    object      Empty                         : FeedUiState
-    object      Reconnect                     : FeedUiState
+    data class  Error   (val msg: String?)                     : FeedUiState
+    data class  Success (val list: List<Feed> = listOf())    : FeedUiState
+    object      Loading                                     : FeedUiState
+    object      Empty                                       : FeedUiState
+    object      Reconnect                                   : FeedUiState
 }
 // @formatter:on
 
