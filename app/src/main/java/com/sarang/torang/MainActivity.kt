344226d062e1a6e694003b1b99681da92d5af1b4
sryang <sarang628@gmail.com>
2025-09-18 08:57:31 +0800

    arrange code

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -3,33 +3,48 @@ package com.sarang.torang
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.sarang.torang.compose.feed.FeedScreen
 import com.sarang.torang.compose.feed.FeedScreenInMain
 import com.sarang.torang.compose.feed.FeedScreenSuccessPreview
 import com.sarang.torang.compose.feed.type.LocalPullToRefreshLayoutType
 import com.sarang.torang.compose.feed.state.rememberFeedScreenState
 import com.sarang.torang.compose.feed.internal.components.LocalExpandableTextType
 import com.sarang.torang.compose.feed.internal.components.LocalFeedImageLoader
+import com.sarang.torang.compose.feed.type.LocalBottomDetectingLazyColumnType
 import com.sarang.torang.compose.feed.type.LocalFeedCompose
+import com.sarang.torang.data.feed.Feed
 import com.sarang.torang.di.basefeed_di.CustomExpandableTextType
 import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
+import com.sarang.torang.di.feed_di.CustomBottomDetectingLazyColumnType
 import com.sarang.torang.di.feed_di.CustomFeedCompose
 import com.sarang.torang.di.feed_di.customPullToRefresh
 import com.sarang.torang.repository.FeedRepository
@@ -42,6 +57,7 @@ import com.sarang.torang.test.TestBasic
 import com.sarang.torang.test.TestFeedScreenByRestaurantId
 import com.sarang.torang.test.TestPinchZoom
 import com.sarang.torang.test.TestUserFeedByReviewIdScreen
+import com.sarang.torang.uistate.FeedUiState
 import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -56,58 +72,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContent {
-            val feedScreenState = rememberFeedScreenState()
-            val scope = rememberCoroutineScope()
             TorangTheme {
-                val navController = rememberNavController()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Scaffold(
-                        floatingActionButton = {
-                            FloatingActionButton({
-                                scope.launch {
-                                    feedScreenState.onTop()
-                                }
-                                scope.launch {
-                                    feedScreenState.showSnackBar("click on top")
-                                }
-                            }) {
-                                Icon(Icons.Default.KeyboardArrowUp, null)
-                            }
-                        }
-                    ) {
-                        CompositionLocalProvider(
-                            LocalFeedCompose provides CustomFeedCompose,
-                            LocalPullToRefreshLayoutType provides customPullToRefresh,
-                            LocalExpandableTextType provides CustomExpandableTextType,
-                            LocalFeedImageLoader provides CustomFeedImageLoader
-                        ){
-                            NavHost(modifier = Modifier.padding(it), navController = navController, startDestination = "FeedScreenInMain") {
-                                //TestBasic()
-                                //FeedScreenSuccessPreview1()
-                                //TestPinchZoom()
-                                //FeedScreenEmptyPreview()
-                                //FeedScreenLoadingPreview()
-                                //TestUserFeedByReviewIdScreen_()
-                                //TestFeedScreenByReviewId_()
-                                //TestFeedScreenByRestaurantId_()
-                                composable("FeedScreenInMain") {
-                                    LaunchedEffect("") {
-                                        feedScreenState.showSnackBar("FeedScreenInMain")
-                                    }
-                                    FeedScreenInMain(feedScreenState = feedScreenState)
-                                }
-                                //LoginRepositoryTest_(loginRepository)
-                                //ProfileRepositoryTest(profileRepository = profileRepository)
-                                //FeedRepositoryTest_(feedRepository = feedRepository)
-                                //TestFeedScreenAndSnackBar()
-                            }
-                        }
-                    }
+                       TextScreen()
                 }
             }
         }
@@ -122,12 +93,88 @@ class MainActivity : ComponentActivity() {
 @Composable fun TestFeedScreenByRestaurantId_() { TestFeedScreenByRestaurantId(234) }
 @Composable fun TestBasic_() { TestBasic() }
 @Composable fun TestPinchZoom_() { TestPinchZoom() }
-// @formatter:on
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable fun TextScreen(){
+    val navController = rememberNavController()
+    val feedScreenState = rememberFeedScreenState()
+    val scope = rememberCoroutineScope()
 
+    CompositionLocalProvider(
+        LocalFeedCompose provides CustomFeedCompose,
+        LocalPullToRefreshLayoutType provides customPullToRefresh,
+        LocalExpandableTextType provides CustomExpandableTextType,
+        LocalFeedImageLoader provides CustomFeedImageLoader,
+        LocalBottomDetectingLazyColumnType provides CustomBottomDetectingLazyColumnType
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "feedScreen"
+        ) {
+            composable("Menu") {
+                Menu(onFeedScreen = {navController.navigate("feedScreen")})
+            }
+            //TestBasic()
+            composable("feedScreen") {
+                var uiState : FeedUiState by remember { mutableStateOf(FeedUiState.Loading) }
+                BottomSheetScaffold(
+                    sheetContent = {
+                        OperationButtons(
+                            onTop = {
+                            scope.launch { feedScreenState.onTop() }
+                            scope.launch { feedScreenState.showSnackBar("click on top") }
+                                    },
+                            onLoading = { uiState = FeedUiState.Loading },
+                            onSuccess = { uiState = FeedUiState.Success(listOf(Feed.Sample,Feed.Sample,Feed.Sample,Feed.Sample,Feed.Sample,Feed.Sample)) }
+
+                        )
+                    }) {
+                    FeedScreen(uiState = uiState, feedScreenState = feedScreenState)
+                }
+            }
+            //TestPinchZoom()
+            //FeedScreenEmptyPreview()
+            //FeedScreenLoadingPreview()
+            //TestUserFeedByReviewIdScreen_()
+            //TestFeedScreenByReviewId_()
+            //TestFeedScreenByRestaurantId_()
+            composable("FeedScreenInMain") {
+                LaunchedEffect("") { feedScreenState.showSnackBar("FeedScreenInMain") }
+                FeedScreenInMain(feedScreenState = feedScreenState)
+            }
+            //LoginRepositoryTest_(loginRepository)
+            //ProfileRepositoryTest(profileRepository = profileRepository)
+            //FeedRepositoryTest_(feedRepository = feedRepository)
+            //TestFeedScreenAndSnackBar()
+        }
+    }
+}
+// @formatter:on
 
 @Preview
 @Composable
-fun FeedScreenSuccessPreview1(){
+fun OperationButtons(
+    onTop: () -> Unit = {},
+    onLoading: () -> Unit = {},
+    onSuccess: () -> Unit = {},
+){
+    Row {
+        AssistChip(modifier = Modifier.padding(4.dp), onClick = onTop, label = { Text("top") })
+        AssistChip(modifier = Modifier.padding(4.dp), onClick = onLoading, label = { Text("loading") })
+        AssistChip(modifier = Modifier.padding(4.dp), onClick = onSuccess, label = { Text("success") })
+    }
+}
+
+@Preview
+@Composable
+fun Menu(onFeedScreen: () -> Unit = {}) {
+    Button(onFeedScreen) {
+        Text("FeedScreen")
+    }
+}
+
+@Preview
+@Composable
+fun FeedScreenSuccessPreview1() {
     CompositionLocalProvider(
         LocalFeedCompose provides CustomFeedCompose,
         LocalPullToRefreshLayoutType provides customPullToRefresh,
