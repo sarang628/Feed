77e93c244067c6f3e89b35636c365b756f8b4014
sryang <sarang628@gmail.com>
2025-09-27 10:08:34 +0800

    type change

--- library/src/main/java/com/sarang/torang/usecase/GetFeedByRestaurantIdFlowUseCase.kt
+++ library/src/main/java/com/sarang/torang/usecase/GetFeedByRestaurantIdFlowUseCase.kt
@@ -1,8 +1,9 @@
 package com.sarang.torang.usecase
 
-import com.sarang.torang.data.feed.Feed
+import com.sarang.torang.uistate.FeedUiState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 interface GetFeedByRestaurantIdFlowUseCase {
-    fun invoke(restaurantId : Int?): Flow<List<Feed>>
+    fun invoke(restaurantId : Int?): Flow<FeedUiState>
 }
