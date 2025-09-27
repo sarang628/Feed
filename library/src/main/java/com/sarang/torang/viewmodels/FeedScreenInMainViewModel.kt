1e8906fdf390c77eb9e51f5f74e1a655717e57a5
sryang <sarang628@gmail.com>
2025-09-27 10:08:23 +0800

    add viewmodel for feed main

---
+++ library/src/main/java/com/sarang/torang/viewmodels/FeedScreenInMainViewModel.kt
@@ -0,0 +1,42 @@
+package com.sarang.torang.viewmodels
+
+import androidx.lifecycle.viewModelScope
+import com.sarang.torang.usecase.ClickFavorityUseCase
+import com.sarang.torang.usecase.ClickLikeUseCase
+import com.sarang.torang.usecase.FeedWithPageUseCase
+import com.sarang.torang.usecase.FindFeedByRestaurantIdFlowUseCase
+import com.sarang.torang.usecase.GetFeedByRestaurantIdFlowUseCase
+import com.sarang.torang.usecase.GetFeedFlowUseCase
+import com.sarang.torang.usecase.GetFeedLodingFlowUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedScreenInMainViewModel @Inject constructor(
+    clickLikeUseCase: ClickLikeUseCase,
+    clickFavorityUseCase: ClickFavorityUseCase,
+    getFeedLoadingFlowUseCase: GetFeedLodingFlowUseCase,
+    getFeedFlowUseCase: GetFeedFlowUseCase,
+    feedWithPageUseCase: FeedWithPageUseCase,
+    private val getFeedByRestaurantIdFlowUseCase: GetFeedByRestaurantIdFlowUseCase,
+    /**
+     * 기존 GetFeedByRestaurantIdFlowUseCase 만 있어서
+     * 로컬 DB에서만 피드를 불러와 리뷰를 제대로 못 가져옴.
+     * 서버에서 불러오는 로직 추가
+     */
+    private val findFeedByRestaurantIdFlowUseCase: FindFeedByRestaurantIdFlowUseCase
+) : FeedsViewModel(
+    feedWithPageUseCase,
+    clickLikeUseCase,
+    clickFavorityUseCase,
+    getFeedLoadingFlowUseCase,
+    getFeedFlowUseCase
+) {
+    init {
+        viewModelScope.launch {
+            feedWithPageUseCase.invoke(0)
+            page = 1
+        }
+    }
+}
