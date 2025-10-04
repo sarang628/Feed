d00001bb2a4dbe21068390999481f86e74b9b455
sryang <sarang628@gmail.com>
2025-10-04 14:32:31 +0900

    update modules

--- app/src/main/java/com/sarang/torang/test/TestPinchZoom.kt
+++ app/src/main/java/com/sarang/torang/test/TestPinchZoom.kt
@@ -1,24 +1,10 @@
 package com.sarang.torang.test
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import com.sarang.torang.compose.feed.FeedScreenInMain
-import com.sarang.torang.compose.feed.type.LocalPullToRefreshLayoutType
-import com.sarang.torang.compose.feed.type.LocalBottomDetectingLazyColumnType
-import com.sarang.torang.compose.feed.internal.components.LocalExpandableTextType
-import com.sarang.torang.compose.feed.internal.components.LocalFeedImageLoader
-import com.sarang.torang.compose.feed.type.LocalFeedCompose
-import com.sarang.torang.di.basefeed_di.CustomExpandableTextType
-import com.sarang.torang.di.feed_di.CustomBottomDetectingLazyColumnType
-import com.sarang.torang.di.feed_di.CustomFeedCompose
-import com.sarang.torang.di.feed_di.customPullToRefresh
-import com.sarang.torang.di.image.provideImageLoader
-import com.sarang.torang.di.pinchzoom.PinchZoomImageBox
-import com.sarang.torang.di.pinchzoom.isZooming
 
 @Composable
 fun TestPinchZoom() {
-    PinchZoomImageBox(provideImageLoader()) { imageLoader, zoomState ->
+    /*PinchZoomImageBox(provideImageLoader()) { imageLoader, zoomState ->
         CompositionLocalProvider(
             LocalFeedImageLoader provides { data -> imageLoader.invoke(data.modifier ,data.url ?: "", data.contentScale, data.height) },
             LocalFeedCompose provides CustomFeedCompose,
@@ -28,5 +14,5 @@ fun TestPinchZoom() {
         ) {
             FeedScreenInMain(pageScrollable = !zoomState.isZooming, scrollEnabled = !zoomState.isZooming)
         }
-    }
+    }*/
 }
