af997f45a99e3c6da9d15e3493e03ff6f920f9f8
sryang <sarang628@gmail.com>
2025-12-22 08:52:05 +0900

    refactor : separate FeedScreen previews

---
+++ library/src/main/java/com/sarang/torang/compose/feed/FeedScreenPreview.kt
@@ -0,0 +1,60 @@
+package com.sarang.torang.compose.feed
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.sarang.torang.compose.feed.component.FeedTopAppBar
+import com.sarang.torang.uistate.FeedLoadingUiState
+import kotlinx.coroutines.delay
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(backgroundColor = 0xFFFDFDF6, showBackground = true)
+@Composable
+fun FeedScreenLoadingPreview() {
+    FeedScreen(/*Preview*/
+        loadingUiState = FeedLoadingUiState.Loading)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(backgroundColor = 0xFFFDFDF6, showBackground = true)
+@Composable
+fun FeedScreenEmptyPreview() {
+    FeedScreen(/*Preview*/
+        loadingUiState = FeedLoadingUiState.Empty)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(backgroundColor = 0xFFFDFDF6, showBackground = true)
+@Composable
+fun FeedScreenSuccessPreview() {
+    FeedScreen(/*Preview*/
+        loadingUiState = FeedLoadingUiState.Success,
+        topAppBar = { FeedTopAppBar() }
+    )
+}
+
+@Preview(backgroundColor = 0xFFFDFDF6, showBackground = true)
+@Composable
+fun TransitionPreview(){
+    var uiState : FeedLoadingUiState by remember { mutableStateOf(FeedLoadingUiState.Loading) }
+
+    LaunchedEffect(uiState) {
+        delay(1000)
+        uiState = FeedLoadingUiState.Success
+    }
+
+    FeedScreen(/*Preview*/
+        loadingUiState = uiState)
+}
+
+@Preview(backgroundColor = 0xFFFDFDF6, showBackground = true)
+@Composable
+fun PreviewReconnect(){
+    FeedScreen(/*Preview*/
+        loadingUiState = FeedLoadingUiState.Reconnect)
+}
